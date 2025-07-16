29087003640a65adde3a0d6d8842c10380fb3471
Mufaqar <mufaqar@gmail.com>
2025-07-16 12:09:10 +0500

    Fleet

---
+++ temp-fleet.php
@@ -0,0 +1,15 @@
+<?php 
+/*Template Name: Fleet*/
+
+get_header(); ?>
+
+
+<?php get_template_part( 'partials/content', 'breadcrumb' ); ?>
+
+<!-- Divider -->
+<div class="divider"></div>
+
+
+
+
+<?php get_footer(); ?>
