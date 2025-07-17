9a9599e149820063ea8b19b46c38dc71fc0eb8d2
Mufaqar <mufaqar@gmail.com>
2025-07-17 15:48:31 +0500

    udpated

--- temp-tours.php
+++ temp-tours.php
@@ -18,6 +18,7 @@ get_header();
 
         <div class="row g-4">
             <div class="col-12 col-md-4">
+
                 <div class="tour-list-sidebar">
                     <div class="sidebar-widget">
                         <h4 class="widget-title mb-4">Category</h4>
@@ -56,6 +57,40 @@ get_header();
                         <!-- Range Slider -->
                         <div id="range-slider-price"></div>
                     </div>
+
+                     <div class="sidebar-widget">
+                        <h4 class="widget-title mb-4">Tour Duration</h4>
+
+                        <?php
+                        $terms = get_terms([
+                            'taxonomy'   => 'tour-duration',
+                            'hide_empty' => false, // Set true to hide empty categories
+                        ]);
+
+                        if (!empty($terms) && !is_wp_error($terms)) :
+                        ?>
+                        <!-- Sidebar Checkbox List -->
+                        <ul class="sidebar-checkbox-list list-unstyled">
+                            <?php foreach ($terms as $term) : ?>
+                            <li>
+                                <div class="form-check">
+                                    <input class="form-check-input tour-filter-checkbox" type="checkbox"
+                                        id="term-<?php echo esc_attr($term->term_id); ?>"
+                                        value="<?php echo esc_attr($term->slug); ?>">
+                                    <label class="form-check-label flex-grow-1 ms-2"
+                                        for="term-<?php echo esc_attr($term->term_id); ?>">
+                                        <?php echo esc_html($term->name); ?>
+                                    </label>
+                                    <span class="text-muted"><?php echo esc_html($term->count); ?></span>
+                                </div>
+                            </li>
+                            <?php endforeach; ?>
+                        </ul>
+                        <?php endif; ?>
+
+                    </div>
+
+                    
                 </div>
             </div>
 
