2e3e6698f91b89a70d736f96e7b19fc07eb59ffc
Mufaqar <mufaqar@gmail.com>
2025-07-18 17:12:14 +0500

    updated

--- 404.php
+++ 404.php
@@ -1,4 +1,26 @@
 <?php get_header(); ?>
-	<h1><?php _e('404 Error, The page you are looking for does not exist','text_domain'); ?></h1> 
-<?php get_sidebar(); ?>
+
+<?php $bg_image =  get_template_directory_uri() . '/assets/img/bg-img/1.jpg';
+
+get_template_part('partials/content', 'breadcrumb', [
+    'bg' => $bg_image
+]); ?>
+<div class="error-section">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 col-sm-10 col-md-8 col-lg-6">
+                <div class="error-content text-center">
+                    <img class="mb-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/core-img/404.png"
+                        alt="">
+                    <p class="mb-5 px-md-5">Sorry, the page you're looking for doesn't exist. If you think something is
+                        broken, report a problem</p>
+                    <a href="<?php bloginfo('url'); ?>" class="btn btn-success">Back To Home <i
+                            class="icon-arrow-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="divider"></div>
+</div>
+
 <?php get_footer(); ?>
