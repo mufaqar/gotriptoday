6fe7d3ce76bbe527c06e66f97805107e6592ff5d
Mufaqar <mufaqar@gmail.com>
2025-07-18 18:19:52 +0500

    updtated

--- single-cars.php
+++ single-cars.php
@@ -18,19 +18,20 @@ get_template_part('partials/content', 'breadcrumb', [
 
     <div class="container">
 
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-     
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
         <div class="hotel-details-header mb-5">
             <div class="row g-5 align-items-end">
-                <div class="col-12 col-lg-4">
+                <div class="col-12 col-lg-6">
                     <h2 class="mb-3">Book your Business Class</h2>
                     <p class="mb-0 d-flex flex-wrap align-items-center gap-2">
-                       
-                        <span><i class="ti ti-car text-success"></i> <?php echo get_post_meta($post->ID, "car_equivalent", true); ?></span>
+
+                        <span><i class="ti ti-car text-success"></i>
+                            <?php echo get_post_meta($post->ID, "car_equivalent", true); ?></span>
                     </p>
                 </div>
 
-                <div class="col-12 col-lg-8">
+                <div class="col-12 col-lg-6">
 
 
                     <div class="d-flex flex-wrap align-items-center justify-content-md-end gap-4">
@@ -40,7 +41,7 @@ get_template_part('partials/content', 'breadcrumb', [
                             <div class="icon">
                                 <i class="ti ti-car-4wd"></i>
                             </div>
-                            <span><?php 
+                            <h2><?php 
 
                   
                        
@@ -56,24 +57,21 @@ get_template_part('partials/content', 'breadcrumb', [
                             }
                                                     
                         
-                        ?></span>
+                        ?></h2>
                         </a>
 
-                  
+
                     </div>
                 </div>
             </div>
         </div>
 
-        <img class="rounded-4" src="<?php echo $bg_image ?>" alt="">
-
-        <div class="divider-sm"></div>
-
         <div class="row g-5">
             <div class="col-12 col-lg-8">
-                <!-- Tour Details Content -->
+       
                 <div class="tour-details-content">
-                    <!-- Hotel Meta Info Card -->
+                      <img class="rounded-4" src="<?php echo $bg_image ?>" alt="">
+                    <!-- Car meta -->
                     <div class="hotel-meta-info-card">
                         <div class="row g-4">
 
@@ -121,12 +119,7 @@ get_template_part('partials/content', 'breadcrumb', [
                         </div>
                     </div>
                     <h2>Overview</h2>
-                    <?php the_content()?>
-
-
-
-
-
+                     <?php the_content()?>
                     <h2>Features & Amenities</h2>
                     <ul class="list-unstyled style-two">
                         <li><i class="ti ti-rosette-discount-check"></i> Meet & Greet included</li>
@@ -134,19 +127,14 @@ get_template_part('partials/content', 'breadcrumb', [
                         <li><i class="ti ti-rosette-discount-check"></i> Free Waiting time</li>
                         <li><i class="ti ti-rosette-discount-check"></i> Safe and secure travel</li>
                     </ul>
-
-
                 </div>
-
-
             </div>
 
             <div class="col-12 col-lg-4">
                 <div class="d-flex flex-column gap-5">
                     <!-- Widget -->
                     <div class="sidebar-widget">
-                       
-                       <?php echo do_shortcode('[jet_fb_form form_id="23745" submit_type="reload" required_mark="*" fields_layout="column" fields_label_tag="div" markup_type="div" enable_progress="" clear=""]')?>
+                        <?php echo do_shortcode('[jet_fb_form form_id="23745" submit_type="reload" required_mark="*" fields_layout="column" fields_label_tag="div" markup_type="div" enable_progress="" clear=""]')?>
                         <!-- Follow  -->
                         <div class="hotel-follow-nav mt-4">
                             <a href="#"><i class="ti ti-brand-facebook"></i></a>
@@ -158,7 +146,7 @@ get_template_part('partials/content', 'breadcrumb', [
                 </div>
             </div>
         </div>
-            <?php endwhile; endif; ?>
+        <?php endwhile; endif; ?>
     </div>
 
     <!-- Divider -->
