6cd93e70bb4a08b89f8804c45f66112f4c593333
Mufaqar <mufaqar@gmail.com>
2025-07-31 21:35:43 +0500

    Booking

--- inc/extra.php
+++ inc/extra.php
@@ -89,3 +89,46 @@ function register_stripe_settings() {
     register_setting('stripe-settings-group', 'stripe_secret_key');
     register_setting('stripe-settings-group', 'stripe_webhook_secret');
 }
+
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Booking.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Booking", "hello-elementor" ),
+		"singular_name" => esc_html__( "Booking", "hello-elementor" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "Booking", "hello-elementor" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "booking", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "booking", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
