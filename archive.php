841cba99ea8f4eaacdc2f0b8b20ed14965bf0dcc
Mufaqar <mufaqar@gmail.com>
2025-08-11 17:42:39 +0500

    updated

--- archive.php
+++ archive.php
@@ -1,48 +1,128 @@
-<?php get_header(); ?>	
-<?php if (have_posts()) : ?>
-<?php $post = $posts[0]; // Hack. Set $post so that the_date() works. ?>
-<?php /* If this is a category archive */ if (is_category()) { ?>
-	<h2><?php _e('Archive for the','text_domain'); ?> &#8216; <?php single_cat_title(); ?> &#8217; <?php _e('Category','text_domain'); ?></h2>
-<?php /* If this is a tag archive */ } elseif( is_tag() ) { ?>
-	<h2><?php _e('Posts Tagged', 'text_domain'); ?> &#8216; <?php single_tag_title(); ?> &#8217;</h2>
-<?php /* If this is a daily archive */ } elseif (is_day()) { ?>
-	<h2><?php _e('Archive for','text_domain'); ?> <?php the_time('F jS, Y'); ?></h2>
-<?php /* If this is a monthly archive */ } elseif (is_month()) { ?>
-	<h2><?php _e('Archive for','text_domain'); ?> <?php the_time('F, Y'); ?></h2>
-<?php /* If this is a yearly archive */ } elseif (is_year()) { ?>
-	<h2><?php _e('Archive for','text_domain'); ?> <?php the_time('Y'); ?></h2>
-<?php /* If this is an author archive */ } elseif (is_author()) { ?>
-	<h2><?php _e('Author Archive','text_domain'); ?> </h2>
-<?php /* If this is a paged archive */ } elseif (isset($_GET['paged']) && !empty($_GET['paged'])) { ?>
-	<h2><?php _e('Blog Archives','text_domain'); ?> </h2>
-<?php } ?>
-<?php while (have_posts()) : the_post(); ?>
-	<div <?php post_class() ?>>
-        <h1 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
-        <?php include (TEMPLATEPATH . '/inc/meta.php' ); ?>
-        <div class="entry">			
-			 <?php if ( has_post_thumbnail() ) { ?>
-                    <div class="thumb">
-						<?php the_post_thumbnail(); ?>
+<?php
+/*
+ * Template Name: Category Archive
+ * Description: A template for displaying posts from a specific category.
+ */
+get_header();
+
+$bg_image = get_theme_mod('category_archive_bg_image') ?: get_template_directory_uri() . '/assets/img/bg-img/97.jpg';
+
+get_template_part('partials/content', 'breadcrumb', [
+    'bg' => $bg_image,
+    'title' => single_cat_title('', false) // Display the category name as the title
+]);
+
+?>
+
+<div class="blog-section">
+    <div class="divider"></div>
+    <div class="container">
+        <div class="row g-5 g-md-4 g-xxl-5">
+            <?php if (have_posts()) : ?>
+                <div class="col-12 col-md-7 col-lg-8">
+                    <?php while (have_posts()) : the_post(); ?>
+                        <div class="single-blog-content wow fadeInUp" data-wow-duration="1000ms" data-wow-delay="200ms">
+                            <!-- Post Thumbnail -->
+                            <div class="col-12 mb-4">
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <img class="rounded-4" src="<?php echo get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>" alt="<?php the_title_attribute(); ?>">
+                                <?php else : ?>
+                                    <img class="rounded-4" src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-img/127.jpg" alt="Default Image">
+                                <?php endif; ?>
+                            </div>
+
+                            <!-- Post Body -->
+                            <div class="post-body">
+                                <div class="blog-meta flex-wrap d-flex align-items-center gap-3 gap-lg-4 mb-3">
+                                    <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+                                        <i class="ti ti-user"></i> By <?php the_author(); ?>
+                                    </a>
+                                    <a href="<?php the_permalink(); ?>">
+                                        <i class="ti ti-calendar"></i> <?php the_date(); ?>
+                                    </a>
+                                    <a href="<?php comments_link(); ?>">
+                                        <i class="ti ti-message-circle"></i>
+                                        <?php comments_number('No Comments', '1 Comment', '% Comments'); ?>
+                                    </a>
+                                </div>
+
+                                <!-- Post Title -->
+                                <h3 class="post-title mb-4"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                                <div class="d-flex flex-column gap-4">
+                                    <?php the_excerpt(); ?>
+                                    <a href="<?php the_permalink(); ?>" class="btn btn-primary">Read More</a>
+                                </div>
+                            </div>
+
+                            <div class="divider-sm"></div>
+
+                            <!-- Tag & Share -->
+                            <div class="tag-share-wrapper">
+                                <ul class="list-unstyled share-list">
+                                    <li>Share:</li>
+                                    <li><a href="#"><i class="ti ti-brand-facebook"></i></a></li>
+                                    <li><a href="#"><i class="ti ti-brand-x"></i></a></li>
+                                    <li><a href="#"><i class="ti ti-brand-linkedin"></i></a></li>
+                                    <li><a href="#"><i class="ti ti-brand-youtube"></i></a></li>
+                                </ul>
+                            </div>
+
+                            <div class="divider-sm"></div>
+                        </div>
+                    <?php endwhile; ?>
+
+                    <!-- Pagination -->
+                    <div class="pagination-wrapper">
+                        <?php
+                        the_posts_pagination([
+                            'prev_text' => '<i class="ti ti-arrow-left"></i>',
+                            'next_text' => '<i class="ti ti-arrow-right"></i>',
+                            'screen_reader_text' => __('Posts navigation')
+                        ]);
+                        ?>
                     </div>
-				<?php } ?>	
-			<?php 
-				global $more;    // Declare global $more (before the loop).
-				$more = 0;       // Set (inside the loop) to display content above the more tag.
-				the_content(__('Continue Reading','text_domain'));
-			?>
-            <div class="clear"></div>
-			<div class="tags">
-			<?php the_tags( __('Tags:','text_domain'),'','.'); ?>
-			</div>
-		</div>
-	</div>
-<?php endwhile; ?>
-<?php if (function_exists("pagination")) {
-	pagination($additional_loop->max_num_pages);
-} ?>			
-<?php else : ?>
-	<h2><?php _e('Nothing Found','text_domain'); ?></h2>
-<?php endif; ?>
-<?php get_sidebar(); ?>
+                </div>
+
+                <!-- Sidebar -->
+                <div class="col-12 col-md-5 col-lg-4">
+                    <div class="d-flex flex-column gap-5">
+                        <!-- Categories Widget -->
+                        <div class="blog-widget">
+                            <div class="h4 fw-bold mb-4">Categories</div>
+                            <ul class="blog-list style-two">
+                                <?php
+                                $categories = get_categories();
+                                foreach ($categories as $category) {
+                                    echo '<li>';
+                                    echo '<a href="' . esc_url(get_category_link($category->term_id)) . '">';
+                                    echo esc_html($category->name);
+                                    echo ' <span>' . $category->count . '</span>';
+                                    echo '</a>';
+                                    echo '</li>';
+                                }
+                                ?>
+                            </ul>
+                        </div>
+
+                        <!-- Follow Us Widget -->
+                        <div class="blog-widget">
+                            <div class="h4 fw-bold mb-4">Follow Us</div>
+                            <div class="follow-nav">
+                                <a href="#"><i class="ti ti-brand-facebook"></i></a>
+                                <a href="#"><i class="ti ti-brand-x"></i></a>
+                                <a href="#"><i class="ti ti-brand-linkedin"></i></a>
+                                <a href="#"><i class="ti ti-brand-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php else : ?>
+                <div class="col-12">
+                    <p><?php esc_html_e('No posts found in this category.', 'text-domain'); ?></p>
+                </div>
+            <?php endif; ?>
+        </div>
+    </div>
+</div>
+
 <?php get_footer(); ?>
