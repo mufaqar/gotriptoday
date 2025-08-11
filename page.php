8c8a0738a0dfe19d2c8ee674a35b4229c7852793
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2025-08-11 18:18:56 +0500

    Page.php

--- page.php
+++ page.php
@@ -9,26 +9,24 @@ get_template_part('partials/content', 'breadcrumb', [
 <!-- Tour Details Section -->
 <div class="tour-details-section">
     <!-- Divider -->
-    <div class="divider"></div>
+    <div class="divider-sm"></div>
 
     <div class="container">
 
         <div class="row g-5">
             <div class="col-12 col-lg-12">
-                <div class="tour-details-content">
-                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-                    <div class="post" id="post-<?php the_ID(); ?>">
-                        <h1><?php the_title(); ?></h1>
-                        <?php the_content(); ?>
-
-                    </div>
-                </div>
-                <?php endwhile; endif; ?>
+                <?php if (have_posts()):
+                    while (have_posts()):
+                        the_post(); ?>
+                        <div class="post" id="post-<?php the_ID(); ?>">
+                            <?php the_content(); ?>
+                        </div>
+                    <?php endwhile; endif; ?>
             </div>
             <!-- Divider -->
-            <div class="divider"></div>
+            <div class="divider-sm"></div>
         </div>
 
     </div>
 </div>
-        <?php get_footer(); ?>
+<?php get_footer(); ?>
