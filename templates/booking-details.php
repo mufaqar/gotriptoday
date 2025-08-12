197e1028d8813a15c9eea7249ba5541c5c6bef0d
Mufaqar <mufaqar@gmail.com>
2025-08-12 15:59:18 +0500

    Booking Page

--- templates/booking-details.php
+++ templates/booking-details.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 }
 ?>
-
+ <div class="divider-sm"></div>
 <div class="booking_details">
     <div class="divider"></div>
     <div class="container">
