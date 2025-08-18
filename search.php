9ec3bae01c88fb23b46151c0be2961ee958a182d
Mufaqar <mufaqar@gmail.com>
2025-08-18 15:25:48 +0500

    Search

--- search.php
+++ search.php
@@ -1,32 +1,27 @@
 <?php get_header(); ?>
-
-<!-- Tour List Section -->
 <section class="tour-list-section">
-    <!-- Divider -->
     <div class="divider"></div>
     <div class="container">
         <div class="row g-4">
-
             <div class="col-12">
                 <div class="tour-list-content">
                     <div id="tour-results" class="row g-4">
                         <?php if (have_posts()) : ?>
                         <h2><?php _e('Search Results','text_domain'); ?></h2>
                         <?php while (have_posts()) : the_post(); 
-        
-                                    echo '<div class="col-12 col-lg-6">';
-                                    get_template_part('partials/tour', 'card'); 
-                                    echo '</div>';
-                             endwhile; ?>
+            
+                                        echo '<div class="col-12 col-lg-6">';
+                                        get_template_part('partials/tour', 'card'); 
+                                        echo '</div>';
+                                endwhile; ?>
                         <?php else : ?>
                         <h2><?php _e('Nothing Found','text_domain'); ?></h2>
                         <?php endif; ?>
 
-                            </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-                        <?php get_footer(); ?>
+<?php get_footer(); ?>
