0f9f015346cd5b4b4b115ee46fd76d8db9751404
Mufaqar <mufaqar@gmail.com>
2025-09-22 15:24:46 +0500

    updated

--- temp-tours.php
+++ temp-tours.php
@@ -2,43 +2,6 @@
 /*Template Name: Tours*/
 get_header();
 ?>
-<div class="breadcrumb-section bg-img jarallax" data-jarallax data-speed="0.6"
-    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/img/bg-img/slide1.webp');">
-    <div class="container">
-        <div class="divider-sm"></div>
-        <div class="search_banner tour_banner mt-5">
-            <h2>Explore around, wherever you are, in one day.</h2>
-            <div class="hero-search-form wow fadeInUp w-full mt-3" data-wow-delay="900ms" data-wow-duration="1000ms">
-                <form class="row align-items-center g-3 g-xxl-2 search-form" role="search" method="get" class=""
-                    action="<?php echo esc_url(home_url('/')); ?>">
-                    <div class="col-12 col-md-8">
-                        <div class="search-item d-flex align-items-center gap-3">
-                            <div class="icon">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
-                                    fill="none">
-                                    <path fill-rule="evenodd" clip-rule="evenodd"
-                                        d="M9.38467 18.4463C9.38467 18.4463 3.33301 13.3496 3.33301 8.33464C3.33301 6.56653 4.03539 4.87083 5.28563 3.62059C6.53587 2.37035 8.23156 1.66797 9.99967 1.66797C11.7678 1.66797 13.4635 2.37035 14.7137 3.62059C15.964 4.87083 16.6663 6.56653 16.6663 8.33464C16.6663 13.3496 10.6147 18.4463 10.6147 18.4463C10.278 18.7563 9.72384 18.753 9.38467 18.4463ZM9.99967 11.2513C10.3827 11.2513 10.762 11.1759 11.1158 11.0293C11.4697 10.8827 11.7912 10.6679 12.0621 10.397C12.3329 10.1262 12.5477 9.80466 12.6943 9.4508C12.8409 9.09693 12.9163 8.71766 12.9163 8.33464C12.9163 7.95161 12.8409 7.57234 12.6943 7.21848C12.5477 6.86461 12.3329 6.54308 12.0621 6.27224C11.7912 6.0014 11.4697 5.78656 11.1158 5.63999C10.762 5.49341 10.3827 5.41797 9.99967 5.41797C9.22613 5.41797 8.48426 5.72526 7.93728 6.27224C7.3903 6.81922 7.08301 7.56109 7.08301 8.33464C7.08301 9.10818 7.3903 9.85005 7.93728 10.397C8.48426 10.944 9.22613 11.2513 9.99967 11.2513Z"
-                                        fill="#767676" />
-                                </svg>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" id="name" class="form-control" placeholder="Explore from"
-                                    value="<?php echo get_search_query(); ?>" name="s">
-                                <input type="hidden" name="post_type" value="tours" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 col-md-4">
-                        <button type="submit" class="btn btn-success w-100">Search Now</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-    <div class="divider-sm"></div>
-</div>
-
-<!-- Hero Section -->
 <section class="hero-section bg-dark">
     <!-- Cloud Image -->
     <div class="cloud-img"></div>
@@ -75,25 +38,37 @@ get_header();
 
     <div class="container">
         <div class="row">
-            <div class="col-12 col-sm-12">
-                <ul class="nav forms-tabs mb-3" id="myTab" role="tablist">
-                    <li class="nav-item" role="presentation">
-                        <button class="tab-link active" id="tab1-tab" data-bs-toggle="tab" data-bs-target="#tab1"
-                            type="button" role="tab" aria-controls="tab1" aria-selected="true">Transfer</button>
-                    </li>
-                    <li class="nav-item" role="presentation">
-                        <a class="tab-link" href="<?php echo home_url('/day-trip'); ?>" type="button">Day Trip</a>
-                    </li>
-                </ul>
-                <div class="tab-content pt-3" id="">
-                    <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
-                        <div class="go_trip_form wow fadeInUp" data-wow-delay="900ms" data-wow-duration="1000ms">
-                            <?php get_template_part('partials/content', 'tab1'); ?>
-                        </div>
-                    </div>
 
+            <div class="search_banner tour_banner mt-5">
+                <h2>Explore around, wherever you are, in one day.</h2>
+                <div class="hero-search-form wow fadeInUp w-full mt-3" data-wow-delay="900ms"
+                    data-wow-duration="1000ms">
+                    <form class="row align-items-center g-3 g-xxl-2 search-form" role="search" method="get" class=""
+                        action="<?php echo esc_url(home_url('/')); ?>">
+                        <div class="col-12 col-md-8">
+                            <div class="search-item d-flex align-items-center gap-3">
+                                <div class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"
+                                        fill="none">
+                                        <path fill-rule="evenodd" clip-rule="evenodd"
+                                            d="M9.38467 18.4463C9.38467 18.4463 3.33301 13.3496 3.33301 8.33464C3.33301 6.56653 4.03539 4.87083 5.28563 3.62059C6.53587 2.37035 8.23156 1.66797 9.99967 1.66797C11.7678 1.66797 13.4635 2.37035 14.7137 3.62059C15.964 4.87083 16.6663 6.56653 16.6663 8.33464C16.6663 13.3496 10.6147 18.4463 10.6147 18.4463C10.278 18.7563 9.72384 18.753 9.38467 18.4463ZM9.99967 11.2513C10.3827 11.2513 10.762 11.1759 11.1158 11.0293C11.4697 10.8827 11.7912 10.6679 12.0621 10.397C12.3329 10.1262 12.5477 9.80466 12.6943 9.4508C12.8409 9.09693 12.9163 8.71766 12.9163 8.33464C12.9163 7.95161 12.8409 7.57234 12.6943 7.21848C12.5477 6.86461 12.3329 6.54308 12.0621 6.27224C11.7912 6.0014 11.4697 5.78656 11.1158 5.63999C10.762 5.49341 10.3827 5.41797 9.99967 5.41797C9.22613 5.41797 8.48426 5.72526 7.93728 6.27224C7.3903 6.81922 7.08301 7.56109 7.08301 8.33464C7.08301 9.10818 7.3903 9.85005 7.93728 10.397C8.48426 10.944 9.22613 11.2513 9.99967 11.2513Z"
+                                            fill="#767676" />
+                                    </svg>
+                                </div>
+                                <div class="form-group">
+                                    <input type="text" id="name" class="form-control" placeholder="Explore from"
+                                        value="<?php echo get_search_query(); ?>" name="s">
+                                    <input type="hidden" name="post_type" value="tours" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-4">
+                            <button type="submit" class="btn btn-success w-100">Search Now</button>
+                        </div>
+                    </form>
                 </div>
             </div>
+
 
         </div>
     </div>
@@ -103,12 +78,14 @@ get_header();
 
 
 
+
+
 <!-- Tour List Section -->
 <section class="tour-list-section">
-       <div class="divider-sm"></div>
+    <div class="divider-sm"></div>
     <!-- Divider -->
     <div class="container">
-        
+
         <div class="row">
             <div class="col-12 col-md-12">
                 <div class="tour-list-content">
