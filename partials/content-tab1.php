c460153cdac0f28b0899e32fababca17eb6b2e11
Mufaqar <mufaqar@gmail.com>
2025-09-29 15:15:41 +0500

    udpated

--- partials/content-tab1.php
+++ partials/content-tab1.php
@@ -1,6 +1,6 @@
 <h2 class="text-white mb-1 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1000ms">Professional Drivers <br/>
 
-    Offering Comfortable Car Transfers.</h2>
+     Comfortable Car Transfers</h2>
 
 
 <?php echo do_shortcode('[chbs_booking_form booking_form_id="10007" widget_mode="1" widget_style="4" widget_booking_form_url=""]') ?>
