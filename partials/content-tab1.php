c39a2a541d17e0f9380e0e6c5c991775544b710a
Mufaqar <mufaqar@gmail.com>
2025-10-01 15:33:20 +0500

    Step 2

--- partials/content-tab1.php
+++ partials/content-tab1.php
@@ -3,4 +3,4 @@
      Comfortable Car Transfers</h2>
 
 
-<?php echo do_shortcode('[chbs_booking_form booking_form_id="10007" widget_mode="1" widget_style="4" widget_second_step=1 widget_booking_form_url="https://gotriptoday.com/booking-page/?"]') ?>
+<?php echo do_shortcode('[chbs_booking_form booking_form_id="10007" widget_mode="1" widget_style="4" widget_second_step=1 widget_booking_form_url="https://gotriptoday.com/booking-page"]') ?>
