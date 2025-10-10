64231e1786cc08de889f8800392702f640023644
Mufaqar <mufaqar@gmail.com>
2025-10-10 15:48:50 +0500

    updated

--- partials/content-tab1.php
+++ partials/content-tab1.php
@@ -1,6 +1,6 @@
-<h2 class="text-white mb-1 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1000ms">Professional Drivers <br/>
+<h1 class="text-white mb-1 wow fadeInUp" data-wow-delay="600ms" data-wow-duration="1000ms">Professional Drivers <br/>
 
-     Comfortable Car Transfers</h2>
+     Comfortable Car Transfers</h1>
 
 
 <?php echo do_shortcode('[chbs_booking_form booking_form_id="10007" widget_mode="1" widget_style="4" widget_second_step=1 widget_booking_form_url="https://gotriptoday.com/booking-page/?"]') ?>
